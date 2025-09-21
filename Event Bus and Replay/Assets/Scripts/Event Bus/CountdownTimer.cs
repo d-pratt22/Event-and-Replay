f89242522710c2ce03b9722f7dcd0bf555f3371f
d-pratt22 <156544542+d-pratt22@users.noreply.github.com>
2025-09-21 14:21:58 -0700

    First

---
+++ Event Bus and Replay/Assets/Scripts/Event Bus/CountdownTimer.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using System.Collections;
+using System.Diagnostics;
+
+namespace Chapter.EventBus
+{
+    public class CountdownTimer : MonoBehaviour
+    {
+        private float _currentTime;
+        private float duration = 3.0f;
+
+        void OnEnable()
+        {
+            RaceEventBus.Subscribe(
+                RaceEventType.COUNTDOWN, StartTimer);
+        }
+
+        void OnDisable()
+        {
+            RaceEventBus.Unsubscribe(
+                RaceEventType.COUNTDOWN, StartTimer);
+        }
+
+        private void StartTimer()
+        {
+            StartCoroutine(Countdown());
+        }
+
+        private IEnumerator Countdown()
+        {
+
+            _currentTime = duration;
+
+            while (_currentTime > 0)
+            {
+                yield return new WaitForSeconds(1f);
+                _currentTime--;
+            }
+
+            RaceEventBus.Publish(RaceEventType.START);
+        }
+
+        void OnGUI()
+        {
+            GUI.color = Color.green;
+            GUI.Label(
+                new Rect(125, 0, 200, 20),
+                "COUNTDOWN: " + _currentTime);
+        }
+    }
+}
